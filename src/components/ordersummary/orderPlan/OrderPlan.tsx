2ab5d6725e3d851186ac05692e17bdd8d8888361
Alan-838 <alanusmp1990@gmail.com>
2026-05-22 11:14:34 -0500

    Se modifico las imagenes de hero y music-icon

--- src/components/ordersummary/orderPlan/OrderPlan.tsx
+++ src/components/ordersummary/orderPlan/OrderPlan.tsx
@@ -1,9 +1,10 @@
-import "./orderPlan.css"
+import "./orderPlan.css";
+import musicIcon from "../../../assets/images/icon-music.svg";
 
 const OrderPlan = ({ price }: { price: string }) => {
     return (
         <div id="orderPlan">
-            <img src="../../src/assets/images/icon-music.svg" alt="icono de musica" />
+            <img src={musicIcon} alt="icono de musica" />
             <div id="orderPlan-text">
                 <h4>Anual Plan</h4>
                 <p> {price}/year</p>
